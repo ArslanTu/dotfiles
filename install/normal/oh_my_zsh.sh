5cccc7e4714e696cf5737020ae4fbec01507dd79
arslantu <arslantu@arslantu.xyz>
2024-01-12 13:42:35 +0800

    install pkgs needed for zsh

--- install/normal/oh_my_zsh.sh
+++ install/normal/oh_my_zsh.sh
@@ -8,12 +8,15 @@ install_oh_my_zsh="${INSTALL_TMP}/install_oh_my_zsh.sh"
 curl "${GITHUB_PROXY}https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh" -o ${install_oh_my_zsh}
 REMOTE="${GITHUB_PROXY}https://github.com/ohmyzsh/ohmyzsh.git" RUNZSH=no sh ${install_oh_my_zsh}
 
+# install dependencies
+sudo apt install -y thefuck fzf
+
 # install plugins
 git clone "${GITHUB_PROXY}https://github.com/zsh-users/zsh-autosuggestions.git" ${HOME}/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone "${GITHUB_PROXY}https://github.com/zsh-users/zsh-syntax-highlighting.git" ${HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone "${GITHUB_PROXY}https://github.com/sobolevn/wakatime-zsh-plugin.git" ${HOME}/.oh-my-zsh/custom/plugins/wakatime
 wget "https://raw.githubusercontent.com/catppuccin/zsh-syntax-highlighting/main/themes/catppuccin_macchiato-zsh-syntax-highlighting.zsh" -O ${HOME}/.oh-my-zsh/catppuccin_macchiato-zsh-syntax-highlighting.zsh
-sudo apt install -y thefuck
+
 # install powerlevel10k
 git clone --depth=1 ${GITHUB_PROXY}https://github.com/romkatv/powerlevel10k.git ${HOME}/.oh-my-zsh/custom/themes/powerlevel10k
 
